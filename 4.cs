73064dddde35e83347a855699cd78a54f3ba623d
SangHoon Hwang <jkae9711@naver.com>
2022-06-16 17:55:09 +0900

    #4 : K번째 수

---
+++ 4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] array, int[,] commands)
+    {
+        int[] answer = new int[50];
+
+        for (int i = 0; i < commands.GetLength(0); i++)
+        {
+            int iStartIndex = commands[i, 0];
+            int iEndIndex = commands[i, 1];
+            int iResultIndex = commands[i, 2];
+
+            int[] arrTempSlice = new int[100];
+
+            Array.Copy(array, iStartIndex - 1,
+                       arrTempSlice, 0, (iEndIndex - iStartIndex + 1));
+
+            arrTempSlice = arrTempSlice.Where(x => x != 0).ToArray();
+
+            Array.Sort(arrTempSlice);
+
+            answer[i] = arrTempSlice[iResultIndex - 1];
+        }
+
+        answer = answer.Where(x => x != 0).ToArray();
+
+        return answer;
+    }
+}

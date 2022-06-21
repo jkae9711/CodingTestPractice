93b021753566e162eb98cbd60b26c4112e63ffad
SangHoon Hwang <jkae9711@naver.com>
2022-06-21 17:16:15 +0900

    #9 : 나누어 떨어지는 숫자 배열

---
+++ 9.cs
@@ -0,0 +1,30 @@
+﻿// #9 : 나누어 떨어지는 숫자 배열
+
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] arr, int divisor)
+    {
+        int[] answer = new int[arr.Length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % divisor == 0)
+            {
+                answer[i] = arr[i];
+            }
+        }
+
+        if (answer.Sum() == 0)
+        {
+            answer[0] = -1;
+        }
+
+        answer = answer.Where(x => x != 0).ToArray();
+        Array.Sort(answer);
+
+        return answer;
+    }
+}

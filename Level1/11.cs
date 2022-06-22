2e8b066667ea1b428914e7202527ce8adf0fdb33
SangHoon Hwang <jkae9711@naver.com>
2022-06-22 14:38:02 +0900

    #11 : 두 정수 사이의 합

---
+++ Level1/11.cs
@@ -0,0 +1,29 @@
+﻿// #11 : 두 정수 사이의 합
+
+public class Solution
+{
+    public long solution(int a, int b)
+    {
+        long answer = 0;
+        int iMin, iMax;
+
+        if (a == b)
+        {
+            answer = a;
+
+            return a;
+        }
+        else
+        {
+            iMin = a < b ? a : b;
+            iMax = a > b ? a : b;
+
+            for (int i = iMin; i <= iMax; i++)
+            {
+                answer += i;
+            }
+
+            return answer;
+        }
+    }
+}

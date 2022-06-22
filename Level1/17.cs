7a950755907f4fa8f023d092d3865668fc66aade
SangHoon Hwang <jkae9711@naver.com>
2022-06-23 08:55:15 +0900

    #17 : 자릿수 더하기

---
+++ Level1/17.cs
@@ -0,0 +1,19 @@
+﻿// #17 : 자릿수 더하기
+
+using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        string strNumber = n.ToString();
+
+        foreach (char item in strNumber)
+        {
+            answer += Convert.ToInt32(item.ToString());
+        }
+
+        return answer;
+    }
+}

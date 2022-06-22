5fea77770d11eb36f8925f849ec4dc75ca517c14
SangHoon Hwang <jkae9711@naver.com>
2022-06-22 15:43:23 +0900

    #14 : 문자열을 정수로 바꾸기

---
+++ Level1/14.cs
@@ -0,0 +1,13 @@
+﻿// #14 : 문자열을 정수로 바꾸기
+
+using System;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = Convert.ToInt32(s);
+
+        return answer;
+    }
+}

661554786136ee074377a7f30184be5010d4350d
SangHoon Hwang <jkae9711@naver.com>
2022-06-22 15:16:33 +0900

    #12 : 수박수박수박수박수박수?

---
+++ Level1/12.cs
@@ -0,0 +1,25 @@
+﻿// #12 : 수박수박수박수박수박수?
+
+using System.Text;
+
+public class Solution
+{
+    public string solution(int n)
+    {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0) // 짝수
+            {
+                answer.Append("수");
+            }
+            else // i % 2 == 1, 홀수
+            {
+                answer.Append("박");
+            }
+        }
+
+        return answer.ToString();
+    }
+}

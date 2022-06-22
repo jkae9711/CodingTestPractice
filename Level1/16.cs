b206220a23cfde6d1281a60b5bf300bd483c7210
SangHoon Hwang <jkae9711@naver.com>
2022-06-22 17:37:42 +0900

    #16 : 약수의 합

---
+++ Level1/16.cs
@@ -0,0 +1,25 @@
+﻿// #16 : 약수의 합
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int iTemp;
+        List<int> lstDivisor = new List<int>();
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (int.TryParse(((double)n / (double)i).ToString(), out iTemp))
+            {
+                lstDivisor.Add(iTemp);
+            }
+        }
+
+        int answer = lstDivisor.ToArray().Sum();
+
+        return answer;
+    }
+}

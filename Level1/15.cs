cda9f671ab105df6c39f6a83c63adadd3d8e1385
SangHoon Hwang <jkae9711@naver.com>
2022-06-22 15:56:40 +0900

    #15 : 문자열 다루기 기본

---
+++ Level1/15.cs
@@ -0,0 +1,21 @@
+﻿// #15 : 문자열 다루기 기본
+
+public class Solution
+{
+    public bool solution(string s)
+    {
+        bool answer = true;
+        int iTemp;
+
+        if (s.Length != 4 && s.Length != 6)
+        {
+            answer &= false;
+        }
+        if (!int.TryParse(s, out iTemp))
+        {
+            answer &= false;
+        }
+
+        return answer;
+    }
+}

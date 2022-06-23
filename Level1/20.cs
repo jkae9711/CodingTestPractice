7c191cc9b01756d0652514a0750621a4cfbd9df4
SangHoon Hwang <jkae9711@naver.com>
2022-06-23 09:51:30 +0900

    Add files via upload

---
+++ Level1/20.cs
@@ -0,0 +1,20 @@
+﻿// #20 : 핸드폰 번호 가리기
+
+using System.Text;
+
+public class Solution
+{
+    public string solution(string phone_number)
+    {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < phone_number.Length - 4; i++)
+        {
+            answer.Append('*');
+        }
+
+        return answer
+            .Append(phone_number.Substring(phone_number.Length - 4, 4))
+            .ToString();
+    }
+}

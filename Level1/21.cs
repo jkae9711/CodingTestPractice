4cb744cd6e0ee6ae73b4570421827405dae7c7fb
SangHoon Hwang <jkae9711@naver.com>
2022-06-23 11:07:05 +0900

    #21 : 직사각형 별찍기

---
+++ Level1/21.cs
@@ -0,0 +1,30 @@
+﻿// #21 : 직사각형 별찍기
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Example
+{
+    public static void Main()
+    {
+        Console.Clear();
+
+        String[] s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        StringBuilder sbRow = new StringBuilder();
+
+        for (int i = 0; i < a; i++)
+        {
+            sbRow.Append("*");
+        }
+
+        for (int i = 0; i < b; i++)
+        {
+            Console.WriteLine(sbRow.ToString());
+        }
+    }
+}

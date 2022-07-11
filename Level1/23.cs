ec3a111c723adeaf573e5775396ef39a5b253d1c
SangHoon Hwang <jkae9711@naver.com>
2022-07-11 17:20:14 +0900

    #23 : 문자열 내림차순으로 배치하기

---
+++ Level1/23.cs
@@ -0,0 +1,18 @@
+﻿// #23 : 문자열 내림차순으로 배치하기
+
+using System.Linq;
+
+public class Solution
+{
+    public string solution(string s)
+    {
+        var lstTemp = (from word in s
+                       orderby word
+                       select word)
+                      .ToList();
+
+        lstTemp.Reverse();
+
+        return new string(lstTemp.ToArray());
+    }
+}

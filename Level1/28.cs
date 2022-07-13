66b75f7180df3c7d380be916a5f40af9e21a19bb
SangHoon Hwang <jkae9711@naver.com>
2022-07-13 13:21:12 +0900

    #28 : 제일 작은 수 제거하기

---
+++ Level1/28.cs
@@ -0,0 +1,16 @@
+﻿// #28 : 제일 작은 수 제거하기
+
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] arr)
+    {
+        int iMin = arr.Min();
+
+        return
+            arr.Length == 1 ?
+            new int[] { -1 } :
+            (from item in arr where item > iMin select item).ToArray();
+    }
+}

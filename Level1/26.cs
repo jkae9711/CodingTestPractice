d78069ad8f461f69487cb3ac60299eecac755551
SangHoon Hwang <jkae9711@naver.com>
2022-07-13 11:45:36 +0900

    #26 : 콜라츠 추측

---
+++ Level1/26.cs
@@ -0,0 +1,23 @@
+﻿// #26 : 콜라츠 추측
+
+public class Solution
+{
+    public int solution(int num)
+    {
+        if (num == 1) return 0;
+
+        ulong iTempOP = (ulong)num;
+        int iCount = 0;
+
+        while(true)
+        {
+            if (iTempOP % 2 == 0) iTempOP /= 2;
+            else iTempOP = ((iTempOP * 3) + 1);
+
+            iCount++;
+
+            if (iCount > 500) return -1;
+            if (iTempOP == 1) return iCount;
+        }
+    }
+}

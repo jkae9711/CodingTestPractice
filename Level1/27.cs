78d9803b679cfc0ef29d723b2ee3913e23362881
SangHoon Hwang <jkae9711@naver.com>
2022-07-13 12:05:12 +0900

    #27 : 정수 제곱근 판별

---
+++ Level1/27.cs
@@ -0,0 +1,14 @@
+﻿// #27 : 정수 제곱근 판별
+
+using System;
+
+public class Solution
+{
+    public long solution(long n)
+    {
+        long lsqrt = 0;
+
+        if (long.TryParse(Math.Sqrt(n).ToString(), out lsqrt) == true) return lsqrt + 1;
+        else return -1;
+    }
+}

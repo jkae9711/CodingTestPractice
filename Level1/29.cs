5ec99796e95852c281ba919d0d680d91e33b6e8c
SangHoon Hwang <jkae9711@naver.com>
2022-07-13 17:29:45 +0900

    #29 : 자연수 뒤집어 배열로 만들기

---
+++ Level1/29.cs
@@ -0,0 +1,15 @@
+﻿// #29 : 자연수 뒤집어 배열로 만들기
+
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(long n)
+    {
+        var answer = from item in n.ToString()
+                     select Convert.ToInt32(item.ToString());
+
+        return answer.Reverse().ToArray();
+    }
+}

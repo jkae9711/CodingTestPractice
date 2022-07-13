c9b5460288ebb9bae11c8457eaf88bb45f19f75f
SangHoon Hwang <jkae9711@naver.com>
2022-07-13 10:57:20 +0900

    #24 : 행렬의 덧셈

---
+++ Level1/24.cs
@@ -0,0 +1,19 @@
+﻿// #24 : 행렬의 덧셈
+
+public class Solution
+{
+    public int[,] solution(int[,] arr1, int[,] arr2)
+    {
+        int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
+
+        for (int col = 0; col < arr1.GetLength(1); col++)
+        {
+            for (int row = 0; row < arr1.GetLength(0); row++)
+            {
+                answer[row, col] = arr1[row, col] + arr2[row, col];
+            }
+        }
+
+        return answer;
+    }
+}

de81a134098a628cfe142021934d158bc5e327e2
SangHoon Hwang <jkae9711@naver.com>
2022-07-13 11:23:07 +0900

    #25 : 하샤드 수

---
+++ Level1/25.cs
@@ -0,0 +1,19 @@
+﻿// #25 : 하샤드 수
+
+using System;
+
+public class Solution
+{
+    public bool solution(int x)
+    {
+        int iSum = 0;
+
+        foreach (var number in x.ToString())
+        {
+            iSum += Convert.ToInt32(number.ToString());
+        }
+
+        if (x % iSum == 0)      return true;
+        else                    return false;
+    }
+}

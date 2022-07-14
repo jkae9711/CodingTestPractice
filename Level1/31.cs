cc04bfaed531fa083156dbbb03bc400b017328cc
SangHoon Hwang <jkae9711@naver.com>
2022-07-14 10:17:40 +0900

    #31 : 정수 내림차순으로 배치하기

---
+++ Level1/31.cs
@@ -0,0 +1,29 @@
+﻿// #31 : 정수 내림차순으로 배치하기
+
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public long solution(long n)
+    {
+        var temp = from value in n.ToString()
+                   orderby value descending
+                   select value.ToString();
+
+        var temp2 = temp.ToArray();
+
+        var temp3 = String.Join("", temp2);
+
+        return Convert.ToInt64(temp3);
+
+        //return
+        //    Convert.ToInt64(
+        //        String.Join("",
+        //        (from value in n.ToString()
+        //        select value.ToString())
+        //        .Reverse()
+        //        .ToArray()
+        //        ));
+    }
+}

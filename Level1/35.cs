7bbf24a2370af4fdd5b3344d7fe1647ad2ee5d24
SangHoon Hwang <jkae9711@naver.com>
2022-07-15 17:38:43 +0900

    #35 : 내적

---
+++ Level1/35.cs
@@ -0,0 +1,20 @@
+﻿// #35 : 내적
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] a, int[] b)
+    {
+        List<int> lstDotProduct = new List<int>();
+
+        for (int index = 0; index < a.Length; index++)
+        {
+            lstDotProduct.Add(a[index] + b[index]);
+        }
+
+        return lstDotProduct.Sum();
+    }
+}

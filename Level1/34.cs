541d29d8760e212be51665cba57e105ec0474016
SangHoon Hwang <jkae9711@naver.com>
2022-07-15 17:12:35 +0900

    #34 : 소수 찾기
    에라토스테네스의 체

---
+++ Level1/34.cs
@@ -0,0 +1,43 @@
+﻿// #34 : 소수 찾기
+
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        List<int> lstPrimeNumber = new List<int>();
+        bool bIsBreak = false;
+
+        for (int number = 2; number <= n; number++)
+        {
+            if (number > 2 && number % 2 == 0)
+                continue;
+
+            foreach (var PrimeNumber in lstPrimeNumber)
+            {
+                if (PrimeNumber > Math.Sqrt(number))
+                    break;
+
+                if (number % PrimeNumber == 0)
+                {
+                    bIsBreak = true;
+
+                    break;
+                }
+            }
+
+            if (bIsBreak)
+            {
+                bIsBreak = false;
+
+                continue;
+            }
+
+            lstPrimeNumber.Add(number);
+        }
+
+        return lstPrimeNumber.Count;
+    }
+}

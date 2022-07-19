c05dce5617f292c42d22ace7a8696f9b078ccaa8
SangHoon Hwang <jkae9711@naver.com>
2022-07-19 15:49:00 +0900

    #36 : 음양 더하기

---
+++ Level1/36.cs
@@ -0,0 +1,22 @@
+﻿// #36 : 음양 더하기
+
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] absolutes, bool[] signs)
+    {
+        int iSum = 0;
+
+        for (int index = 0; index < absolutes.Length; index++)
+        {
+            if (signs[index] == true)
+                iSum += absolutes[index];
+            else
+                iSum -= absolutes[index];
+        }
+
+        return iSum;
+    }
+}

078103c0c631443e3ae0e4e7ef2273fe22f67e5f
SangHoon Hwang <jkae9711@naver.com>
2022-07-19 17:59:42 +0900

    #37 : 예산

---
+++ Level1/37.cs
@@ -0,0 +1,31 @@
+﻿// #37 : 예산
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] d, int budget)
+    {
+        d = (from money in d
+            orderby money
+            select money)
+            .ToArray();
+
+        int iAcceptCount = 0;
+
+        foreach (var money in d)
+        {
+            if (money <= budget)
+            {
+                iAcceptCount++;
+                budget -= money;
+            }
+            else
+                break;
+        }
+
+        return iAcceptCount;
+    }
+}

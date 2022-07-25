b7a04c42106e74a24498b16e195a8dd7d0095f6f
SangHoon Hwang <jkae9711@naver.com>
2022-07-26 08:53:03 +0900

    #38 : 없는 숫자 더하기

---
+++ Level1/38.cs
@@ -0,0 +1,13 @@
+﻿// #38 : 없는 숫자 더하기
+
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] numbers)
+    {
+        return Enumerable.Range(0, 10)
+                .Except(numbers)
+                .Sum();
+    }
+}

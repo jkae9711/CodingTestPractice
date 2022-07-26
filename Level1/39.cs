55e24b8d3647af57f873a94e6854c0e9168b0c45
SangHoon Hwang <jkae9711@naver.com>
2022-07-26 09:35:09 +0900

    #39 : 부족한 금액 계산하기

---
+++ Level1/39.cs
@@ -0,0 +1,21 @@
+﻿// #39 : 부족한 금액 계산하기
+
+class Solution
+{
+    public long solution(int price, int money, int count)
+    {
+        long lPriceSum = 0;
+        long lPrice = price;
+        long lMoney = money;
+
+        for (long lCount = 1; lCount <= count; lCount++)
+            lPriceSum += lPrice * lCount;
+
+        if (lPriceSum - lMoney <= 0)
+            return 0;
+        else
+            return lPriceSum - lMoney;
+
+        // 등차수열의 합을 사용할수도 있음
+    }
+}

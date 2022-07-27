789e6ba11b63ae284e65a03e714c8fd1b6495903
SangHoon Hwang <jkae9711@naver.com>
2022-07-27 11:49:07 +0900

    #42 : 나머지가 1이 되는 수 찾기

---
+++ Level1/42.cs
@@ -0,0 +1,22 @@
+﻿// #42 : 나머지가 1이 되는 수 찾기
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int iInput = n - 1;
+        int iAnswer = 0;
+
+        for (int i = 2; i <= iInput; i++)
+        {
+            if (iInput % i == 0)
+            {
+                iAnswer = i;
+
+                break;
+            }
+        }
+
+        return iAnswer;
+    }
+}
